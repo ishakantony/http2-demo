fca7f2c9adceab6a637ff20dd2c0714b4af89c56
Ishak Antony Darmawan <ishak.antony@yahoo.com>
2021-01-02 17:47:20 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// Import entrypoint for your styling
+import './scss/app.scss'
+
+// Import entrypoint for your app logic
+import './app'
+
+// Don't code any logic here, this file is just for entrypoint
