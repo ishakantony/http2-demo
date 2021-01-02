45308aef7a6fe38821d995d42de52e0036b40316
Ishak Antony Darmawan <ishak.antony@yahoo.com>
2021-01-02 18:18:15 +0800

    Add statistic and button to switch between http 1 and 2

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
 // Do your logic here
+const loadTimeEl = document.querySelector('#loadingTime')
+const goToHttp1El = document.querySelector('#goToHttp1')
+const goToHttp2El = document.querySelector('#goToHttp2')
+const initDate = Date.now()
+
+goToHttp1El.addEventListener('click', () => {
+  const href = window.location.href
+  window.location.href = 'http' + href.substring(href.indexOf(':'))
+})
+
+goToHttp2El.addEventListener('click', () => {
+  const href = window.location.href
+  window.location.href = 'https' + href.substring(href.indexOf(':'))
+})
+
+window.addEventListener('load', () => {
+  const milliseconds = Date.now() - initDate
+  loadTimeEl.innerHTML = milliseconds / 1000 + 's'
+})
